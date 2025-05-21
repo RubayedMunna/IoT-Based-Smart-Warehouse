4ffe865e9133ca0a8d2284dda25f5fabf6e8b58c
Akila-Nipo <akila.nipo1104@gmail.com>
2025-05-21 20:57:10 +0600

    Changed title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from './components/Dashboard';
 function App() {
   return (
     <div className="container mt-5">
-      <h1 className="text-center mb-4">ThingSpeak IoT Monitor</h1>
+      <h1 className="text-center mb-4">ThingSpeak IoT Monitor(s)</h1>
       <Dashboard />
     </div>
   );
